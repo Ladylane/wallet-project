00317a33fa816c0e3da62034da6eb30957aaf605
Ladylane <ladylanesousa@gmail.com>
2022-04-13 21:54:18 -0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.1a4232ece0acf2a0272d44d32975a1df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6296353c1396bed68f8e4192cc1cea7e",
+    "url": "/wallet-project/index.html"
+  },
+  {
+    "revision": "0c935346bb6ba6004d03",
+    "url": "/wallet-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "548203cf50654fe97ce3",
+    "url": "/wallet-project/static/js/2.74906dbb.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/wallet-project/static/js/2.74906dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975dc4175b58f9a10ee3",
+    "url": "/wallet-project/static/js/3.4b8f9af1.chunk.js"
+  },
+  {
+    "revision": "0c935346bb6ba6004d03",
+    "url": "/wallet-project/static/js/main.45091c58.chunk.js"
+  },
+  {
+    "revision": "9a48e87b6af3b430c918",
+    "url": "/wallet-project/static/js/runtime-main.5f51edb0.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/wallet-project/static/media/logo.06e73328.svg"
+  }
+]);
